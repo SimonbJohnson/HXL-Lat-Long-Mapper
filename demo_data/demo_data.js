51a602560630416a4ae65fff2b677f41e0bfe51e
SimonbJohnson <simonbjohnson@gmail.com>
2014-09-29 14:18:19 +0100

    Demo data corrections

--- demo_data/demo_data.js
+++ demo_data/demo_data.js
@@ -9,7 +9,7 @@ var demoData = "Status	Country	Primary Organisation	Type	Latitude	Longitude \r\n
 "Functional	Liberia	MOH	Triage	6.32445	-10.8073 \r\n"+
 "Functional	Liberia	MOH	ETC	6.28723	-10.77325 \r\n"+
 "Functional	Liberia	MOH	Triage	6.3697076	-10.7919744 \r\n"+
-"Funtional	Liberia	MOH	ETC	6.3843716	-10.7871135 \r\n"+
+"Functional	Liberia	MOH	ETC	6.3843716	-10.7871135 \r\n"+
 "Pending	Liberia		ETC	6.30892	-10.80411 \r\n"+
 "Pending	Liberia		ETC	6.25697	-10.70202 \r\n"+
 "Pending	Liberia		ETC	6.26969	-10.7347 \r\n"+
@@ -27,19 +27,19 @@ var demoData = "Status	Country	Primary Organisation	Type	Latitude	Longitude \r\n
 "Pending	Liberia	No Partner Identified	ETC	5.8161533	-8.0995537 \r\n"+
 "Functional	Guinea	MSF	ETC	9.53564	-13.6832 \r\n"+
 "Functional	Guinea	MSF	ETC	8.56152	-10.13236 \r\n"+
-"Functional	Guinea	MOH	Transit Centre	8.53688	-9.46481 \r\n"+
+"Functional	Guinea	MOH	Holding Centre	8.53688	-9.46481 \r\n"+
 "Closed	Guinea	MSF	ETC	10.9115	-13.0299 \r\n"+
-"Pending	Guinea	N/A	Transit Centre	7.7621	-8.8143 \r\n"+
-"Pending	Guinea	N/A	Transit Centre	7.5695786	-9.2597491 \r\n"+
+"Pending	Guinea	N/A	Holding Centre	7.7621	-8.8143 \r\n"+
+"Pending	Guinea	N/A	Holding Centre	7.5695786	-9.2597491 \r\n"+
 "Functional	Guinea	MSF	Triage	10.74207	-11.10646 \r\n"+
 "Functional	Guinea	MSF	Triage	9.19844	-10.10352 \r\n"+
-"Pending	Guinea	No Partner Identified	Transit Centre	9.4312135	-13.0918656 \r\n"+
+"Pending	Guinea	No Partner Identified	Holding Centre	9.4312135	-13.0918656 \r\n"+
 "Functional	Sierra Leone	MOH	ETC	7.87517	-11.18473 \r\n"+
 "Functional	Sierra Leone	IFRC	ETC	7.9390302	-11.1422825 \r\n"+
 "Functional	Sierra Leone	MSF	ETC	8.27685	-10.56647 \r\n"+
 "Functional	Sierra Leone	MSF	Triage	8.4616449	-10.3363192 \r\n"+
 "Functional	Sierra Leone	MSF	Triage	8.2808305	-10.372805 \r\n"+
-"Functional	Sierra Leone	MSF	Trage	7.4941267	-11.1693745 \r\n"+
+"Functional	Sierra Leone	MSF	Triage	7.4941267	-11.1693745 \r\n"+
 "Functional	Sierra Leone	King's Health Partners UK	Triage	8.48823	-13.2384 \r\n"+
 "Pending	Sierra Leone	No Partner Identified	Triage	8.4790017	-13.2680158 \r\n"+
 "Functional	Sierra Leone		Triage	8.387303	-13.1387201 \r\n"+
